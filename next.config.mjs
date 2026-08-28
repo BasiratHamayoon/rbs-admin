36cc9616c52b250a5ae774b634fb6fdf24eac3bf
BasiratHamayoon <basirathumayun@gmail.com>
2026-08-28 16:03:03 +0500

    Adding some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['res.cloudinary.com'], // Add your Cloudinary domain
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
